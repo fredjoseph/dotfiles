b83e5965fa638e1328b228a61c662c5e88da16bb
Frederic JOSEPH <fredjoseph@users.noreply.github.com>
2020-12-21 16:22:42 +0100

    Update home configuration

---
+++ kde/.local/share/konsole/Default.profile
@@ -0,0 +1,11 @@
+[Appearance]
+ColorScheme=BriseDark
+Font=Hack,10,-1,0,75,0,0,0,0,0,Bold
+
+[General]
+Name=Default
+Parent=FALLBACK/
+
+[Interaction Options]
+AutoCopySelectedText=true
+MiddleClickPasteMode=1
---
+++ scripts/settings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install the plugins without waiting for the first launch
+vi -E -s +PlugInstall +visual +qall >/dev/null 2>&1
+
+# Yakuake
+echo "
+[Window]
+ShowTabBar=false" >> ~/.config/yakuakerc
+
+# Konsole
+sed -i -e "/DefaultProfile=/cDefaultProfile=Default.profile" ~/.config/konsolerc
+
+# Change Keyboard Layout
+sed -i -e "/LayoutList=/cLayoutList=fr(azerty),gb(extd)" ~/.config/kxkbrc
