47c3132e32779f47c52acfd5b1ad3395e0445479
fredjoseph <fredjoseph@users.noreply.github.com>
2021-12-11 15:00:15 +0100

    Update misc_apps.sh

--- scripts/misc_apps.sh
+++ scripts/misc_apps.sh
@@ -45,8 +45,7 @@ install_zsh_plugins() {
   __get_local_zsh_plugin 'https://raw.githubusercontent.com/lukechilds/zsh-nvm/master/zsh-nvm.plugin.zsh'
   local completion_date=$(curl -s "https://api.github.com/repos/zsh-users/zsh-completions/commits?path=src/_nvm&page=1&per_page=1" | jq -r '.[0].commit.committer.date')
   local my_completion=$(git log -1 --date=short --format="%ad" -- $(dirname "$0")/../nvm/.zsh-custom/completions/_nvm)
-  # FIXME: to be tested and switch
-  if [[ $my_completion > $completion_date ]]; then print_warning "The completion file for '_nvm' seems outdated"; fi
+  if [[ $my_completion < $completion_date ]]; then print_warning "The completion file for '_nvm' seems outdated"; fi
   print_arrow "fast-syntax-highlighting"
   __clone_local_zsh_plugin 'https://github.com/zdharma-continuum/fast-syntax-highlighting.git'
 }
