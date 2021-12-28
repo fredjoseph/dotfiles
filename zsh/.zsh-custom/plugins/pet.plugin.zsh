46c22c04c4b92af509fa2a2836fb4a7f53d7790b
Frederic JOSEPH <fredjoseph@users.noreply.github.com>
2021-12-28 15:52:40 +0100

    Update configuration

---
+++ zsh/.zsh-custom/plugins/pet.plugin.zsh
@@ -0,0 +1,9 @@
+# List all snippets with 'ctrl-s'
+function pet-select() {
+  BUFFER=$(pet search --query "$LBUFFER")
+  CURSOR=$#BUFFER
+  zle redisplay
+}
+zle -N pet-select
+stty -ixon
+bindkey '^s' pet-select
